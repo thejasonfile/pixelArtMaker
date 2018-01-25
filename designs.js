a6a85b876ea2f6ec43216bedd8cbd3e4d22e5b8b
Jason Arnold <jason.arnold.00@gmail.com>
2018-01-25 18:34:24 -0500

    added missing semicolons and comments per the style guide

--- designs.js
+++ designs.js
@@ -23,29 +23,35 @@ $(function() {
   }
 
   function makeGrid() {
-    //clear the previous grid
+    // clear the previous grid
     clearGrid();
 
-    //get input values for width and height
+    // get input values for width and height
     let rows = getWidthValue();
     let cols = getHeightValue();
 
-    //nested loop to build grid and append it to table
+    // nested loop to build grid and append it to table
     for (let r = 0; r < rows; r++) {
-      $('#pixel_canvas').append(`<tr class="row${r}"></tr>`)
+      $('#pixel_canvas').append(`<tr class='row${r}'></tr>`);
       for (let c = 0; c < cols; c++) {
-        $(`.row${r}`).append(`<td></td>`)
+        $(`.row${r}`).append(`<td></td>`);
       }
     }
   }
 
   // EVENT LISTENERS
-
+  
+  // clicking the submit button builds the grid
   $('input[type="submit"]').click(function(e) {
+    /**
+    * the default behavior for a submit button is to refresh the page
+    * this needs to be prevented
+    **/
     e.preventDefault();
-    makeGrid()
+    makeGrid();
   });
 
-  $('#pixel_canvas').on('click', 'td', fillSquare)
+  // clicking a grid square changes the background color
+  $('#pixel_canvas').on('click', 'td', fillSquare);
 
 });
